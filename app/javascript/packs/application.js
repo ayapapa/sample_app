70be4d46334be3a496377b49f6cec6ea0f414e79
ayapapa <ayapapajapan@yahoo.co.jp>
2021-05-05 12:22:35 +0900

    add styles and links witout log-in

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+//import 'bootstrap'
+//import '../stylesheets/application'
+//import './bootstrap_custom.js'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
